ba6d11610df366f542c11366220ac2020b489839
Mazhar Alam <mazharalamov@gmail.com>
2021-11-24 09:20:22 +0530

    assignment/middleware2

--- src/controllers/orderConlroller.js
+++ src/controllers/orderConlroller.js
@@ -1,31 +1,53 @@
-const orderModel= require("../models/orderModel")
-const userModel= require("../models/userModel")
-const productModel= require("../models/productModel")
+const orderModel = require("../models/orderModel")
+const userModel = require("../models/userModel")
+const productModel = require("../models/productModel")
 
 
 
 const createmyorder = async function (req, res) {
-    
-let orderdata = req.body;
-let uid = req.body.userId;
-let pid = req.body.productId;
-let validateuid = await userModel.findById(uid);
-let validatepid = await productModel.findById(pid);
-let isfreeapp = req.headers.isfreeapp
 
+    let orderdata = req.body;
+    let uid = req.body.userId;
+    let pid = req.body.productId;
+    let user = await userModel.findById(uid);
+    let product = await productModel.findById(pid);
+    //let isfreeapp = req.headers.isfreeapp
+    let appTypeFree = req.isFreeAppUser
+    let orderamount
+    let date =Date()
+    //user validation
+    if ( !user) {
+        return res.send({ message: "User doesn't exist. Please provide a valid userId" })
+    }
+    //product validation
+    if (!product) {
+        return res.send({ message: "User doesn't exist. Please provide a valid productId" })
+    }
+    //for free app user
+    if (appTypeFree) {
+        orderamount = 0
+    } else {
+        orderamount = product.price
+    }
+    //user balance validation
+    if (!appTypeFree && user.balance < product.price) {
+        return res.send({ message: "User doesn't have enough balance to purchase the product" })
+    }
+    let orderdetails={
+        userId: uid ,
+        productId:pid, 
+        amount:  orderamount,
+        isFreeAppUser:appTypeFree,
+        date:date
+    }
+    let orderCreated = await orderModel.create(orderdetails)
+    if(!appTypeFree){
+        {
+            await userModel.findOneAndUpdate({_id: uid}, {balance: user.balance - product.price})
+        }
+    }
+    res.send({data: orderCreated})
 
-if(validateuid && validatepid){
-    var data= orderdata
-    let savedData= await orderModel.create(data)
-    res.send({msg: savedData})
-}else{
-
-    res.send({msg:"plz enter valid user id & product id"})
 
 }
-
-
-
-
-}
-module.exports.createmyorder= createmyorder
+module.exports.createmyorder = createmyorder
--- src/middlewares/headerMw.js
+++ src/middlewares/headerMw.js
@@ -1,27 +1,20 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const mid= function(req,res,next){
-    let headerdata=req.headers.isfreeapp;
-   
-    if(headerdata){
-        next()
-    }else{
+const mid = function (req, res, next) {
+    let headerdata = req.headers.isfreeapp;
+    let isAppFree
+    if (!headerdata) {
         res.send("Plz check your header")
+
     }
-   
+    if (headerdata == 'true') {
+
+        req.isAppFree = true;
+    } else {
+
+        req.isAppFree = false
+    }
+    req.isFreeAppUser = isAppFree
+    next()
 }
-
-const mid2=function(req,res,next){
-let headerdt=req.headers.isfreeapp;
-if(headerdt==true){
-    req.body.amount=0 ;
-    req.body.save()
-    req.body.isFreeAppUser = true;
-    req.body.save()
-
-}else {
-
-}
-
-}
-module.exports.mid=mid
+module.exports.mid = mid
