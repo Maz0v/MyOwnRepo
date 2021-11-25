5fe6ef7f462d06b984d207c8196fd91c818aaaf6
Mazhar Alam <mazharalamov@gmail.com>
2021-11-25 20:31:09 +0530

    axios Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ router.get("/cowin/states", cowinController.getStatesList)
 router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
 router.get("/cowin/centers", cowinController.getByPin)
 router.post("/cowin/getOtp", cowinController.getOtp)
-
-
+router.get("/london/weather", cowinController.london)
+router.get("/city/temp", cowinController.londonTemp)
+router.get("/citytemp/temp", cowinController.Temp)
 module.exports = router;
